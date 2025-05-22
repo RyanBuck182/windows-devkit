9cfe8d2df369bcb8422439a89506752a0f6a805d
ryanbuck182@ <ryanbuck182@gmail.com>
2025-05-22 16:30:58 -0400

    Add command shortcuts for windows terminal

---
+++ commands/tc.bat
@@ -0,0 +1,4 @@
+:: Open a new command prompt tab in the current windows terminal window.
+
+@echo off
+wt --window last nt --profile "Command Prompt"
---
+++ commands/tp.bat
@@ -0,0 +1,4 @@
+:: Open a new powershell tab in the current windows terminal window.
+
+@echo off
+wt --window last nt --profile "Windows Powershell"
