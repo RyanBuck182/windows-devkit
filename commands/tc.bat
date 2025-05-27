30e70c4acd80d81ac1a87b4df29fcd4c88bb6e0d
RyanBuck182 <RyanBuck182@gmail.com>
2025-05-27 10:22:07 -0400

    Add todo messages

--- commands/tc.bat
+++ commands/tc.bat
@@ -1,4 +1,5 @@
 :: Open a new command prompt tab in the current windows terminal window.
+:: TODO: make it open a new tab when run in administrator window
 
 @echo off
 wt --window last nt --profile "Command Prompt" %*
--- commands/tp.bat
+++ commands/tp.bat
@@ -1,4 +1,5 @@
 :: Open a new powershell tab in the current windows terminal window.
+:: TODO: make it open a new tab when run in administrator window
 
 @echo off
 wt --window last nt --profile "Windows Powershell" %*
