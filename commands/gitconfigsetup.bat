fa5789cac3e814cf11a606a321f6acc82e871b3f
RyanBuck182 <RyanBuck182@gmail.com>
2025-06-01 02:47:19 -0400

    Reorder aliases

--- commands/gitconfigsetup.bat
+++ commands/gitconfigsetup.bat
@@ -19,45 +19,38 @@ git config --global diff.colorMoved default
 git config --global --replace-all alias.aliasconfig "config --global --replace-all"
 
 git aliasconfig alias.s "status"
+git aliasconfig alias.a "add"
+git aliasconfig alias.d "diff"
+git aliasconfig alias.ds "diff --staged"
+git aliasconfig alias.rs "restore --staged"
 
 git aliasconfig alias.c "commit"
 git aliasconfig alias.cm "commit -m"
 git aliasconfig alias.ca "commit --amend"
 git aliasconfig alias.cam "commit --amend -m"
 
-git aliasconfig alias.a "add"
-
-git aliasconfig alias.d "diff"
-git aliasconfig alias.ds "diff --staged"
-
 git aliasconfig alias.l "log --oneline"
 git aliasconfig alias.lg "log --oneline --graph"
 
-git aliasconfig alias.rs "restore --staged"
-
 git aliasconfig alias.sw "switch"
 git aliasconfig alias.swc "switch -c"
-
 git aliasconfig alias.b "branch"
 
-:: Branch list by last commit
-git aliasconfig alias.bl "branch --sort=-committerdate"
-
 git aliasconfig alias.mg "merge"
-
 git aliasconfig alias.rb "rebase"
 git aliasconfig alias.rbi "rebase -i"
 
 git aliasconfig alias.f "fetch"
 git aliasconfig alias.fp "fetch --prune"
-
 git aliasconfig alias.pl "pull"
+git aliasconfig alias.ps "push"
 
 :: Pull with merge/rebase
 git aliasconfig alias.plm "-c pull.ff=true pull"
 git aliasconfig alias.plr "-c pull.ff=true pull --rebase"
 
-git aliasconfig alias.ps "push"
+:: Branch list by last commit
+git aliasconfig alias.bl "branch --sort=-committerdate"
 
 :: Pushing a new branch
 git aliasconfig alias.psu "push -u origin"
