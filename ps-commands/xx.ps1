b5880a6788fc34ad0624ea9f8f7ed8502aa78c5d
ryanbuck182 <ryanbuck182@gmail.com>
2025-06-24 19:42:00 -0400

    Update comments

--- ps-commands/xx.ps1
+++ ps-commands/xx.ps1
@@ -1,4 +1,6 @@
 # Alias for exit
 # Didn't wanna do just 'x' to avoid accidents
+# This file isn't actually used cause of technical reasons
+# The command's implementation is in setup/generate-wrappers and setup/populate-profile
 
 exit
