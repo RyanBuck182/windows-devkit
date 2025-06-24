397f2e7271b147a22297ecc80017b083fcc77608
ryanbuck182 <ryanbuck182@gmail.com>
2025-06-24 17:45:26 -0400

    Generate batch wrappers for the powershell commands

---
+++ setup/generate-wrappers.ps1
@@ -0,0 +1,37 @@
+# Generates batch wrappers for the powershell commands
+
+$commandDir = Join-Path $PSScriptRoot "..\ps-commands"
+$wrapperDir = Join-Path $PSScriptRoot "..\build\bat-wrappers"
+
+# Create dir if doesn't exist
+[System.IO.Directory]::CreateDirectory($wrapperDir)
+
+# Clear wrappers
+Get-ChildItem -Path $wrapperDir -File | Remove-Item
+
+# Function to build the wrapper content
+function New-WrapperContent($commandPath) {
+return @"
+:: Wrapper for "$commandPath"
+@echo off
+powershell -ExecutionPolicy Bypass -File "$commandPath" %*"
+"@
+}
+
+# Create wrappers
+Get-ChildItem -Path $commandDir -Filter *.ps1 | ForEach-Object {
+    $commandName = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+    $commandPath = $_.FullName
+
+    $wrapperContent = New-WrapperContent $commandPath
+    $wrapperPath = Join-Path $wrapperDir "$commandName.bat"
+
+    Set-Content -Path $wrapperPath -Value $wrapperContent
+}
+
+# Have to do something special for exit cause it's a keyword not a command
+Set-Content -Path (Join-Path $wrapperDir "xx.bat") -Value @"
+:: Alias for exit
+@echo off
+exit
+"@
