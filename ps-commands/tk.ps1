3b9d895e1ef32e23a47338d582d06a2ae56fc0ef
ryanbuck182 <ryanbuck182@gmail.com>
2025-06-24 20:53:56 -0400

    Rename tk to tp and tp to to

--- ps-commands/tk.ps1
+++
@@ -1,4 +0,0 @@
-# Open a new powershell tab in the current windows terminal window.
-# Starts in the same directory as where the script is being run from.
-
-wt --window last nt --profile "Windows Powershell" --startingDirectory $PWD.Path @args
---
+++ ps-commands/to.ps1
@@ -0,0 +1,3 @@
+# Open a new powershell tab in the current windows terminal window.
+
+wt --window last nt --profile "Windows Powershell" @args
