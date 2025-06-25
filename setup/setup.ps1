40487add03bdf28bc21073ffe282b6835ad9ba26
ryanbuck182 <ryanbuck182@gmail.com>
2025-06-24 20:04:54 -0400

    Move setup script into setup folder

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -3,13 +3,13 @@ Write-Host "Once you have done so, please press enter..."
 Read-Host | Out-Null
 
 Write-Host "Setting git config..."
-& .\setup\git-config-setup.ps1
+& .\git-config-setup.ps1
 
 Write-Host "Adding commands to powershell profile..."
-& .\setup\populate-profile.ps1
+& .\populate-profile.ps1
 
 Write-Host "Generating batch wrappers..."
-& .\setup\generate-wrappers.ps1
+& .\generate-wrappers.ps1
 
 Write-Host "Adding batch wrappers to the PATH environment variable..."
-& .\setup\add-wrappers-to-path.ps1
+& .\add-wrappers-to-path.ps1
