0cf666f211ffd7b3b039e11cd3940d8a6758561c
ryanbuck182 <ryanbuck182@gmail.com>
2025-07-17 11:57:17 -0400

    Make psha open fewer windows

--- resources/ps-commands/psha.ps1
+++ resources/ps-commands/psha.ps1
@@ -2,4 +2,14 @@
 # This is a workaround for this bug https://github.com/microsoft/terminal/issues/9628
 # which prevents the tp command from working correctly in an administrator window.
 
-Start-Process -Verb RunAs powershell.exe -ArgumentList '-Command', 'wt -w new nt'
+# Check if script is admin
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$scriptIsAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if ($scriptIsAdmin) {
+    # Start new ps instance to fix bug
+    wt -w new nt
+} else {
+    # Start new ps instance as admin, then start another instance to fix bug
+    Start-Process -Verb RunAs powershell.exe -ArgumentList '-Command', 'wt -w new nt'
+}
