4c74a54a7da7bc1c46e173363b42ebe7f56fc922
ryanbuck182 <ryanbuck182@gmail.com>
2025-09-08 13:42:37 -0400

    Ensure ExecutionPolicy is always set correctly

--- resources/setup/populate-profile.ps1
+++ resources/setup/populate-profile.ps1
@@ -1,6 +1,7 @@
 $commandDir = Join-Path $PSScriptRoot "..\ps-commands"
 $injectFile = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot "..\..\build\ps-profile\inject.ps1"))
 $userProfile = $PROFILE.CurrentUserAllHosts
+$executionPolicy = Get-Content (Join-Path $PSScriptRoot ".\ExecutionPolicy.ps1") -Raw
 
 # Create inject dir if doesn't exist
 [System.IO.Directory]::CreateDirectory((Split-Path -Path $injectFile -Parent)) | Out-Null
@@ -26,10 +27,12 @@ Set-Content -Path $injectFile -Value $injectContent
 $extraInjects = & (Join-Path $PSScriptRoot ".\profile-additions.ps1")
 
 # Add boundary markers to injection code
+# Ensure execution policy allows scripts
 $beginMarker = "# BEGIN DEVKIT COMMAND INJECTION"
 $endMarker = "# END DEVKIT COMMAND INJECTION"
 $injectionBlock = @"
 $beginMarker
+$executionPolicy
 . '$injectFile'
 $extraInjects
 $endMarker
--- setup.bat
+++ setup.bat
@@ -1,2 +1,3 @@
 @echo off
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser" -NoProfile
 powershell -ExecutionPolicy Bypass -File "%~dp0resources\setup\setup.ps1" -NoProfile %*
