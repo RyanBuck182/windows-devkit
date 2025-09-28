0a3befca2c4cccc577621d891ab91fdb2274b39c
ryanbuck182 <ryanbuck182@gmail.com>
2025-09-28 19:52:44 -0400

    Fix syntax error

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser" -NoProfile
-powershell -ExecutionPolicy Bypass -File "%~dp0resources\setup\setup.ps1" -NoProfile %*
+powershell -NoProfile -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0resources\setup\setup.ps1" %*
