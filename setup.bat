03d4eeae34af2a1c29183ab5173b347dd80b911f
ryanbuck182 <ryanbuck182@gmail.com>
2025-10-03 20:20:22 -0400

    Ignore execution policy change errors

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoProfile -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
+powershell -NoProfile -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -ErrorAction SilentlyContinue"
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0resources\setup\setup.ps1" %*
